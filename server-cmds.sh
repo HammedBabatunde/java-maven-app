44a9f7ce4e47e65218f36afc1dd2860582b9cb93
Hammed Babatunde <dev_babs@pop-os.localdomain>
2022-08-19 13:10:54 +0100

    Updated Jenkins file and added script

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export IMAGE=hammedbabatunde/demo-app:java-maven-2.0 
-sudo docker-compose -f docker-compose.yaml up -d
+docker-compose -f docker-compose.yaml up -d
 echo "success"
